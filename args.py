66d46cb483f330f4b17a0d20ac2be273fec2d4c4
R-Itoga <ryo.itoga@outlook.com>
2025-10-06 07:31:18 +0900

    Add files via upload

---
+++ args.py
@@ -0,0 +1,107 @@
+# Copyright 2018,2019,2020,2021 Sony Corporation.
+# Copyright 2021 Sony Group Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def get_args(monitor_path='tmp.monitor', max_epoch=200, model_save_path=None,
+             learning_rate=2*1e-4,
+             batch_size=2, description=None):
+    """
+    Get command line arguments.
+
+    Arguments set the default values of command line arguments.
+    """
+    import argparse
+    import os
+    if model_save_path is None:
+        model_save_path = monitor_path
+    if description is None:
+        description = ("NNabla implementation of CycleGAN. The following help shared among examples in this folder. "
+                       "Some arguments are valid or invalid in some examples.")
+    parser = argparse.ArgumentParser(description)
+    parser.add_argument("--batch-size", "-b", type=int, default=batch_size)
+    parser.add_argument("--learning-rate", "-l",
+                        type=float, default=learning_rate)
+    parser.add_argument("--monitor-path", "-m",
+                        type=str, default=monitor_path,
+                        help='Path monitoring logs saved.')
+    parser.add_argument("--max-epoch", "-e", type=int, default=max_epoch,
+                        help='Max epoch of training. Epoch is determined by the max of the number of images for two domains.')
+    parser.add_argument("--device-id", "-d", type=int, default=0,
+                        help='Device ID the training run on. This is only valid if you specify `-c cudnn`.')
+    parser.add_argument("--model-save-path", "-o",
+                        type=str, default=model_save_path,
+                        help='Path where model parameters are saved.')
+    parser.add_argument("--model-load-path",
+                        type=str,
+                        help='Path where model parameters are loaded.')
+    parser.add_argument("--dataset",
+                        type=str, default="GANdatasets", choices=["ae_photos",
+                                                                  "apple2orange",
+                                                                  "cezanne2photo",
+                                                                  "cityscapes",
+                                                                  "facades",
+                                                                  "horse2zebra",
+                                                                  "iphone2dslr_flower",
+                                                                  "maps",
+                                                                  "monet2photo",
+                                                                  "summer2winter_yosemite",
+                                                                  "ukiyoe2photo",
+                                                                  "vangogh2photo"
+                                                                  "GANdatasets"],
+                        help='Dataset to be used.')
+    parser.add_argument('--context', '-c', type=str,
+                        default=None, help="Extension modules. ex) 'cpu', 'cudnn'.")
+    parser.add_argument("--type-config", "-t", type=str, default='float',
+                        help='Type configuration (float or half)')
+    parser.add_argument('--lambda-recon', type=float,
+                        default=10.,
+                        help="Coefficient for reconstruction loss.")
+    parser.add_argument('--lambda-idt', type=float,
+                        default=3,
+                        help="Coefficient for identity loss. Default is 0, but set 0.5 to comply with the pytorch cycle-gan implementation.")
+    parser.add_argument('--unpool', action='store_true')
+    parser.add_argument('--init-method', default=None, type=str,
+                        help="`None`|`paper`")
+    parser.add_argument('--resume-from', type=str, default=None,
+                    help='Path to the checkpoint directory to resume training from.')
+
+    args = parser.parse_args()
+    if not os.path.isdir(args.model_save_path):
+        os.makedirs(args.model_save_path)
+    return args
+
+
+def save_args(args):
+    from nnabla import logger
+    import os
+    import datetime # <--- import を追加
+
+    if not os.path.exists(args.monitor_path):
+        os.makedirs(args.monitor_path)
+
+    path = "{}/Arguments.txt".format(args.monitor_path)
+    
+    # 追記モード 'a' を使用し、ファイルが存在すれば追記、なければ新規作成
+    with open(path, "a", encoding="utf-8") as fp: # <--- "w" から "a" に変更、エンコーディングも指定
+        
+        # 追記時に区切り線とタイムスタンプを挿入
+        fp.write("\n" + "="*30 + "\n")
+        fp.write(f"Run started at: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        fp.write("="*30 + "\n")
+
+        logger.info("Arguments are saved to {}.".format(path))
+        for k, v in sorted(vars(args).items()):
+            logger.info("{}={}".format(k, v))
+            fp.write("{}={}\n".format(k, v))
---
+++ test.py
@@ -0,0 +1,157 @@
+# Copyright 2018,2019,2020,2021 Sony Corporation.
+# Copyright 2021 Sony Group Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import numpy as np
+import argparse
+import nnabla as nn
+import nnabla.logger as logger
+import nnabla.functions as F
+import nnabla.parametric_functions as PF
+import nnabla.solvers as S
+from nnabla.monitor import Monitor
+from nnabla.ext_utils import get_extension_context
+
+from args import get_args, save_args
+from cycle_gan_data import cycle_gan_data_source, cycle_gan_data_iterator
+import models
+from helpers import MonitorImageWithName
+
+
+def test(args):
+    # Settings
+    b = args.batch_size # コマンドライン引数からバッチサイズを取得
+    c, h, w = 3, 256, 256
+    beta1 = 0.5
+    beta2 = 0.999
+    lambda_recon = args.lambda_recon
+    lambda_idt = args.lambda_idt
+    base_lr = args.learning_rate
+    init_method = args.init_method
+
+    # Context
+    extension_module = args.context
+    if args.context is None:
+        extension_module = 'cpu'
+    logger.info("Running in %s" % extension_module)
+    ctx = get_extension_context(extension_module,
+                                device_id=args.device_id, type_config=args.type_config)
+    nn.set_default_context(ctx)
+
+    # Inputs
+    x_real_test = nn.Variable([b, c, h, w], need_grad=False)
+    y_real_test = nn.Variable([b, c, h, w], need_grad=False)
+
+    # Models for test
+    model_path = args.model_load_path
+    # 指定されたパスがディレクトリの場合、その中のparams.h5を読み込む
+    if os.path.isdir(model_path):
+        model_path = os.path.join(model_path, 'params.h5')
+
+    logger.info(f"Loading test model from: {model_path}")
+    nn.load_parameters(model_path)
+    y_fake_test = models.g(
+        x_real_test, unpool=args.unpool, init_method=init_method)
+    x_fake_test = models.f(
+        y_real_test, unpool=args.unpool, init_method=init_method)
+    y_fake_test.persistent, x_fake_test.persistent = True, True
+    # Reconstruct
+    x_recon_test = models.f(
+        y_fake_test, unpool=args.unpool, init_method=init_method)
+    y_recon_test = models.g(
+        x_fake_test, unpool=args.unpool, init_method=init_method)
+
+    # Datasets
+    rng = np.random.RandomState(313)
+    ds_test_B = cycle_gan_data_source(
+        args.dataset, train=False, domain="B", shuffle=False, rng=rng)
+    ds_test_A = cycle_gan_data_source(
+        args.dataset, train=False, domain="A", shuffle=False, rng=rng)
+    di_test_B = cycle_gan_data_iterator(ds_test_B, args.batch_size)
+    di_test_A = cycle_gan_data_iterator(ds_test_A, args.batch_size)
+
+    # Monitors
+    monitor = Monitor(args.monitor_path)
+
+    def make_monitor_image(name):
+        # return MonitorImageWithName(name, monitor, interval=1,
+        #                         normalize_method=lambda x: (x + 1.0) * 127.5)
+        return MonitorImageWithName(name, monitor, interval=1,
+                                    normalize_method=lambda x: x + 1.0)
+    monitor_test_gx = make_monitor_image('fake_images_test_A')
+    monitor_test_fy = make_monitor_image('fake_images_test_B')
+    monitor_test_x_recon = make_monitor_image('fake_images_recon_test_B')
+    monitor_test_y_recon = make_monitor_image('fake_images_recon_test_A')
+
+    # Validation for B
+    logger.info("Validation for B")
+    # 不適切なループ回数を修正し、全件をカバーできるように変更
+    for i in range((di_test_A.size + args.batch_size - 1) // args.batch_size):
+        y_data, _ = di_test_A.next()
+        y_real_test.d = y_data
+        y_recon_test.forward(clear_buffer=True)
+        
+        # バッチ内の各画像についてループ処理
+        for j in range(y_data.shape[0]):
+            file_index = i * args.batch_size + j
+            # データセットのサイズを超えたらスキップ（最後のバッチ対策）
+            if file_index >= di_test_A.size:
+                continue
+            
+            name = ds_test_A.filename_list[file_index]
+            logger.info("generating a fake of {}".format(name))
+            
+            # 1枚ずつ画像データを抜き出して保存
+            fake_b_image = np.expand_dims(x_fake_test.d[j], axis=0)
+            recon_a_image = np.expand_dims(y_recon_test.d[j], axis=0)
+            
+            monitor_test_fy.add(name, fake_b_image)
+            monitor_test_y_recon.add(name, recon_a_image)
+
+    # Validation for A
+    logger.info("Validation for A")
+    # 不適切なループ回数を修正し、全件をカバーできるように変更
+    for i in range((di_test_B.size + args.batch_size - 1) // args.batch_size):
+        x_data, _ = di_test_B.next()
+        x_real_test.d = x_data
+        x_recon_test.forward(clear_buffer=True)
+
+        # バッチ内の各画像についてループ処理
+        for j in range(x_data.shape[0]):
+            file_index = i * args.batch_size + j
+            # データセットのサイズを超えたらスキップ（最後のバッチ対策）
+            if file_index >= di_test_B.size:
+                continue
+            
+            name = ds_test_B.filename_list[file_index]
+            logger.info("generating a fake of {}".format(name))
+
+            # 1枚ずつ画像データを抜き出して保存
+            fake_a_image = np.expand_dims(y_fake_test.d[j], axis=0)
+            recon_b_image = np.expand_dims(x_recon_test.d[j], axis=0)
+
+            monitor_test_gx.add(name, fake_a_image)
+            monitor_test_x_recon.add(name, recon_b_image)
+
+
+def main():
+    args = get_args()
+    save_args(args)
+    test(args)
+
+
+if __name__ == '__main__':
+    main()
